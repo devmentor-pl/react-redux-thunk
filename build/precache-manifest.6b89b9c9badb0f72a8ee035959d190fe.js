976122ba612787d35382fd91321b55943a322412
Mateusz Bogolubow <mateusz@bogolubow.com>
2020-06-15 08:37:45 +0200

    Add production files

---
+++ build/precache-manifest.6b89b9c9badb0f72a8ee035959d190fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56129dec4c2574fb0d0b10c4f043e16d",
+    "url": "/react-redux-thunk/build/index.html"
+  },
+  {
+    "revision": "afcc0dc09ef2e51cfa5a",
+    "url": "/react-redux-thunk/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8bec893c1fef4fbeeeff",
+    "url": "/react-redux-thunk/build/static/js/2.6b1e90b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-thunk/build/static/js/2.6b1e90b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afcc0dc09ef2e51cfa5a",
+    "url": "/react-redux-thunk/build/static/js/main.57c46f92.chunk.js"
+  },
+  {
+    "revision": "bbc136f89e52dbbad204",
+    "url": "/react-redux-thunk/build/static/js/runtime-main.94d41ac8.js"
+  }
+]);
