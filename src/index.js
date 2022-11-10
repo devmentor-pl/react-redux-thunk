848fa3d04c5e058d5978395c992ca0f14e14b2d7
devmentor.pl <55095257+devmentor-pl@users.noreply.github.com>
2022-11-10 12:35:01 +0100

    update to React v.18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // ./src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import IP from './IP';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -14,16 +14,18 @@ const store = createStore(
     applyMiddleware(thunk),
 );
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
-        <Provider store={ store }>
+        <Provider store={store}>
             <IP />
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+reportWebVitals();
+
